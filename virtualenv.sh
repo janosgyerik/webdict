e649c0cf32c783567f60fa58843f7257e11e4800
Janos Gyerik <janos@kronos>
2014-07-18 22:46:09 +0200

    added convenience scripts for running and virtualenv

---
+++ virtualenv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+projectname=$(basename "$PWD")
+virtualenv=$(dirname "$BASH_SOURCE")/virtualenv
+if test -d $virtualenv; then
+    . $virtualenv/bin/activate
+else
+    echo virtualenv does not exist: $virtualenv >&2
+    echo Create it with: virtualenv --distribute $virtualenv >&2
+    echo Better yet, run ./scripts/init.sh >&2
+fi
