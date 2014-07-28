213df7a7a36195110173d111d0c1dd3007204bc6
Janos Gyerik <janos@kronos>
2014-07-28 22:23:26 +0200

    added a restful handler

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from flask import Flask, request, render_template
+from flask.ext.restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 import simplejson as json
 import english
@@ -26,6 +28,26 @@ def query():
         return json.dumps({'words': words, 'similar': similar, }, indent=4)
     elif filename:
         return json.dumps({'words': [english.get(filename)], 'similar': [], }, indent=4)
+
+
+class SearchByExact(Resource):
+    def get(self, keyword):
+        words = [x for x in english.find(keyword)]
+        if len(words):
+            similar = []
+        else:
+            similar = english.similar(keyword)
+        return json.dumps({'words': words, 'similar': similar, }, indent=4)
+
+
+'''
+/search/exact
+/search/prefix
+/search/suffix
+/search/partial
+/entry
+'''
+api.add_resource(SearchByExact, '/search/exact/<string:keyword>')
 
 
 if __name__ == '__main__':
