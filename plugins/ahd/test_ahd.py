c1abf98b41e107e1a88a9035d3da08572d2bacb6
Janos Gyerik <janos@kronos>
2014-08-04 23:20:01 +0200

    ahd tests passing

--- plugins/ahd/test_ahd.py
+++ plugins/ahd/test_ahd.py
@@ -13,58 +13,44 @@ class AmericanHeritageDictionaryTestCase(unittest.TestCase):
     def setUp(self):
         self.dict = test_dict
 
-    def find(self, word):
-        return self.dict.lookup(word)
-
-    def assert_matched_count(self, count, word):
-        self.assertEqual(count, len(self.find(word)))
+    def assert_matched_count(self, count, results):
+        self.assertEqual(count, len(results))
 
     def test_find_none(self):
-        self.assert_matched_count(0, "nonexistent")
+        self.assert_matched_count(0, self.dict.lookup("nonexistent"))
 
     def test_find_by_exact_single(self):
-        self.assert_matched_count(0, "nonexistent")
-
-    def test_sanity(self):
-        #TODO
-        print(self.dict.lookup('sound'))
-        print(self.dict.lookup('soundx'))
-        print(self.dict.lookup_by_prefix('knee'))
-        print(self.dict.lookup_by_suffix('sound'))
-        print(self.dict.lookup_by_fragment('hello'))
-        for item in self.dict.lookup('sound'):
-            print(item)
-        print(self.dict.lookup('hello'))
+        self.assert_matched_count(1, self.dict.lookup("hello"))
 
     def test_find_by_exact_many(self):
-        pass
+        self.assert_matched_count(4, self.dict.lookup("sound"))
 
     def test_find_by_prefix_none(self):
-        pass
+        self.assert_matched_count(0, self.dict.lookup_by_prefix("kneex"))
 
     def test_find_by_prefix_single(self):
-        pass
+        self.assert_matched_count(1, self.dict.lookup_by_prefix("hello"))
 
     def test_find_by_prefix_many(self):
-        pass
+        self.assert_matched_count(14, self.dict.lookup_by_prefix("knee"))
 
     def test_find_by_suffix_none(self):
-        pass
+        self.assert_matched_count(0, self.dict.lookup_by_suffix("kneex"))
 
     def test_find_by_suffix_single(self):
-        pass
+        self.assert_matched_count(1, self.dict.lookup_by_suffix("hello"))
 
     def test_find_by_suffix_many(self):
-        pass
+        self.assert_matched_count(8, self.dict.lookup_by_suffix("sound"))
 
     def test_find_by_fragment_none(self):
-        pass
+        self.assert_matched_count(0, self.dict.lookup_by_fragment("kneex"))
 
     def test_find_by_fragment_single(self):
-        pass
+        self.assert_matched_count(1, self.dict.lookup_by_fragment("vanguard"))
 
     def test_find_by_fragment_many(self):
-        pass
+        self.assert_matched_count(3, self.dict.lookup_by_fragment("hello"))
 
 
 def test_main():
