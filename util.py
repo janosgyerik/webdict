68d7bc2bfc11e91a410e56bf655470965a202ea9
Janos Gyerik <janos@kronos>
2014-08-14 23:02:54 +0200

    moved import plugins to the top

--- util.py
+++ util.py
@@ -1,12 +1,14 @@
 import os
+import plugins
+
 from imp import find_module, load_module
+
 
 BASE_DIR = os.path.dirname(__file__)
 PLUGINS_PATH = os.path.join(BASE_DIR, 'plugins')
 
 
 def discover_dictionaries():
-    import plugins
     for plugin_name in os.listdir(PLUGINS_PATH):
         plugin_path = os.path.join(PLUGINS_PATH, plugin_name, plugin_name + '.py')
         if os.path.isfile(plugin_path):
