94d3253a138df8c635b655834b7ddcf66e9dbf28
Janos Gyerik <janos@kronos>
2014-08-14 23:37:41 +0200

    no need to import plugins

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import os
-import plugins
 
 from imp import find_module, load_module
 
@@ -13,7 +12,7 @@ def discover_dictionaries():
         plugin_path = os.path.join(PLUGINS_PATH, plugin_name, plugin_name + '.py')
         if os.path.isfile(plugin_path):
             try:
-                fp, pathname, description = find_module(plugin_name, plugins.__path__)
+                fp, pathname, description = find_module(plugin_name, [PLUGINS_PATH])
                 load_module(plugin_name, fp, pathname, description)
                 module = load_module(plugin_name, *find_module(plugin_name, [pathname]))
                 yield plugin_name, getattr(module, 'Dictionary')()
